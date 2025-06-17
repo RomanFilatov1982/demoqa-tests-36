98d3d4ee542574e7ed6c87586041a55d1bfd50aa
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-06-17 10:42:01 +0300

    changed all

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -8,44 +8,52 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
+        Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 5000;
     }
 
     @Test
     void fillFormTests(){
-        open("/automation-practice-form");
-        $("#firstName").setValue("Roman");
-        $("#lastName").setValue("Filatov");
-        $("#userEmail").setValue("romanf@gmail.com");
-        $("[for=gender-radio-1]").click();
-        //$("[for=gender-radio-1]").shouldBe(checked);
-        //$("[for=gender-radio-2]").shouldNotBe(checked);
-        //$("[for=gender-radio-3]").shouldNotBe(checked);
-        $("#userNumber").setValue("9085693730");
-        $(".react-datepicker-wrapper").click();
-        //$("select.react-datepicker__month-select").shouldBe(visible).click();
-        $("select.react-datepicker__month-select").shouldBe(visible).selectOption("January");
-        //$("select.react-datepicker__year-select").shouldBe(visible).click();
-        $("select.react-datepicker__year-select").shouldBe(visible).selectOption("1982");
-        $$(".react-datepicker__day").findBy(text("18")).click();
-        $("#subjectsInput").setValue("Fisics");
-        $("label[for=hobbies-checkbox-1]").click();
+        open("/automation-practice-form"); // правильно
+
+        $("#firstName").setValue("Roman"); //правильно
+
+        $("#lastName").setValue("Filatov"); //правильно
+
+        $("#userEmail").setValue("romanf@gmail.com"); //правильно
+
+        $("#genterWrapper").$(byText("Male")).click(); //правильно
+        //$("[for=gender-radio-1]").click(); //правильно
+        //$("#gender-radio-1").parent().click(); //правильно
+        //$(byText("Other")).click(); //правильно
+
+        $("#userNumber").setValue("9085693730"); //правильно
+
+        $("#dateOfBirthInput").click(); //правильно
+        $(".react-datepicker__month-select").selectOption("January"); // правильно
+        $("select.react-datepicker__year-select").selectOption("1982"); //
+        $(".react-datepicker__day--018").click(); //:not(.react-datepicker__day--outside-month)
+
+        $("#subjectsInput").setValue("Fisics").pressEnter();
+
+        $("#hobbiesWrapper").$(byText("Sports")).click(); //правильно
+
         //$("#uploadPicture").uploadFile(new File("path/to/Screenshot_46.png"));
         File file = new File("src/test/resources/Screenshot_46.png");
         $("#currentAddress").setValue("Proxladnaya street 28");
         //$(".css-yk16xz-control").click();
-        $(".css-19bqh2r").click();
-        $(".css-11unzgr").shouldBe(visible);
-        $$(".css-11unzgr div").findBy(text("Haryana")).click();
+        $("#state").click();
+        $(".css-1pahdxg-control").shouldBe(visible);
+        $$(". css-1uccc91-singleValue").findBy(text("Haryana")).click();
         $("#city").click();
         $(".css-11unzgr").shouldHave(Condition.text("Karnal"));
         $(".css-11unzgr div").shouldHave(Condition.exactText("Karnal"));
