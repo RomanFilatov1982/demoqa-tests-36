2913faa24635f63e422ba94b05342e75031d27a1
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-06-25 15:13:00 +0300

    changed methods dragAndDropTest and actionsTest

--- src/test/java/celenide2/DragAndDropChangeOnAction.java
+++ src/test/java/celenide2/DragAndDropChangeOnAction.java
@@ -1,6 +1,7 @@
 package celenide2;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,17 +16,26 @@ public class DragAndDropChangeOnAction {
         Configuration.browserSize = "1920x1080";
     }
     @Test
-    void DragAndDropTest() {
+    void dragAndDropTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
 
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+
         $("#column-a").dragAndDrop(to("#column-b"));
+    }
+    @Test
+    void actionsTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
 
         SelenideElement source = $("#column-a");
         SelenideElement target = $("#column-b");
+
         $(source).shouldHave(text("A"));
         $(target).shouldHave(text("B"));
 
-        actions().clickAndHold(source).moveToElement(target).release().perform();
+        Selenide.actions().clickAndHold(source).moveToElement(target).release().perform();
+
         $(source).shouldHave(text("B"));
         $(target).shouldHave(text("A"));
     }
