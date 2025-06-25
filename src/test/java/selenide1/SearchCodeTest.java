8d036c15a64e1babc67be249acd170d803aab34d
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-06-25 18:13:14 +0300

    changed package

--- src/test/java/selenide1/SearchCodeTest.java
+++ src/test/java/selenide1/SearchCodeTest.java
@@ -1,4 +1,4 @@
-package celenide1;
+package selenide1;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
--- src/test/java/selenide2/DragAndDropChangeOnAction.java
+++ src/test/java/selenide2/DragAndDropChangeOnAction.java
@@ -1,4 +1,4 @@
-package celenide2;
+package selenide2;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
